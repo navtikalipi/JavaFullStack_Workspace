03d38f8379bcca154eb4abb0315e04bb43ca61d0
Navtika Lipi <navtikalipi2002@gmail.com>
2026-02-06 15:20:23 +0530

    Added OOPs

---
+++ JavaProject/src/Waiter.java
@@ -0,0 +1,10 @@
+class Waiter {
+
+    void takeOrder(String dishName) {
+        System.out.println("Waiter takes order for " + dishName);
+    }
+
+    void serveDish(String dishName) {
+        System.out.println("Waiter serves " + dishName + " to the customer");
+    }
+}
